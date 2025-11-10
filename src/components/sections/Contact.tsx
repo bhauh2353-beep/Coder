19208830d92807768c846a1836520fcfcc04fbbb
Hari Bhau <bhauh2353@gmail.com>
2025-11-10 10:23:12 +0000

    but when user send message then Error please check the form for error

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -72,7 +72,7 @@ const Contact = () => {
   }
 
   return (
-    <section id="contact" className="relative w-full py-8 md:py-12 overflow-hidden">
+    <section id="contact" className="relative w-full py-2 md:py-4 overflow-hidden">
         {backgroundImage && (
             <Image
                 src={backgroundImage.imageUrl}
