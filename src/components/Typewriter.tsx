41be13e51ef0948d122ba8bfff59176e60622ee2
Hari Bhau <bhauh2353@gmail.com>
2025-11-10 18:36:43 +0000

    also the headline gap reduce

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -63,7 +63,7 @@ const Typewriter = ({
   return (
     <h1 className={cn(className)}>
       {displayedText}
-      <span className={cn('ml-1 inline-block h-10 w-1 bg-foreground blinking-cursor', cursorClassName)}></span>
+      <span className={cn('ml-1 inline-block h-8 w-1 bg-foreground blinking-cursor', cursorClassName)}></span>
     </h1>
   );
 };
