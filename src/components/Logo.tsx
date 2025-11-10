9c595ae2c0df1da3117b30bed915efb691f4ad45
Hari Bhau <bhauh2353@gmail.com>
2025-11-10 18:06:01 +0000

    reduce more

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -33,7 +33,7 @@ const Logo = ({ className }: LogoProps) => {
   if (!isMounted || isLoading) {
     return (
       <div className={cn('flex items-center', className)}>
-        <Skeleton className="h-12 w-20 rounded-md" />
+        <Skeleton className="h-12 w-16 rounded-md" />
         <Skeleton className="h-6 w-32" />
       </div>
     );
@@ -42,7 +42,7 @@ const Logo = ({ className }: LogoProps) => {
   return (
     <Link href="/" className={cn('flex items-center px-0', className)}>
       {logoUrl ? (
-        <div className="relative h-12 w-20">
+        <div className="relative h-12 w-16">
           <Image
             src={logoUrl}
             alt={`${companyName} Logo`}
